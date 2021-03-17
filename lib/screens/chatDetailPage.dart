d2f1e70215e437613b81b0c6546e38c9a9b4151f
James Dindi <dindijjames@gmail.com>
2021-03-17 21:21:35 +0300

    Custom AppBar

--- lib/screens/chatDetailPage.dart
+++ lib/screens/chatDetailPage.dart
@@ -10,7 +10,56 @@ class _ChatDetailPageState extends State<ChatDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat Detail'),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.only(right: 16.0),
+            child: Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back, color: Colors.black,),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }
+                ),
+                SizedBox(width: 2.0,),
+                CircleAvatar(
+                  backgroundImage: NetworkImage('https://uifaces.co/our-content/donated/yn7PKqTT.jpg'),
+                  maxRadius: 20.0,
+                ),
+                SizedBox(width: 12.0,),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Kriss Benwat',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600
+                        ),
+                      ),
+                      Text(
+                        'Online',
+                        style: TextStyle(
+                          color: Colors.grey.shade600,
+                          fontSize: 13.0
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Icon(
+                  Icons.settings,
+                  color: Colors.black54,
+                )
+              ],
+            ),
+          ),
+        ),
       ),
       body: Container(),
     );
